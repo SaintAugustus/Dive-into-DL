0285c638109758e4126e8a2aef8c3a11cd1eb413
SaintAugustus <saintaugustus2020@gmail.com>
2023-05-18 16:55:36 -0500

    softmax regression

--- utils/Models.py
+++ utils/Models.py
@@ -1,4 +1,5 @@
 import torch
+import torch.nn as nn
 
 
 def linreg(X, w, b):
@@ -10,5 +11,17 @@ def softmax(X):
     partition = X_exp.sum(1, keepdim=True)
     return X_exp / partition
 
-def softmax_net(X, W, b):
-    return softmax(torch.matmul(X.reshape((-1, W.shape[0])), W) + b)
+class SoftmaxNet:
+    def __init__(self, W, b):
+        self.W = W
+        self.b = b
+
+    def forward(self, X):
+        return softmax(torch.matmul(X.reshape((-1, self.W.shape[0])), self.W) + self.b)
+
+
+def init_weights(m):
+    if type(m) == nn.Linear:
+        nn.init.normal_(m.weight, std=0.01)
+
+
--- utils/Optim.py
+++ utils/Optim.py
@@ -9,21 +9,25 @@ def squared_loss(y_hat, y):
     return (y_hat - y.reshape(y_hat.shape)) ** 2 / 2
 
 def cross_entropy(y_hat, y):
-    return -torch.log(y_hat[range(y_hat), y])
+    return -torch.log(y_hat[range(len(y_hat)), y])
 
 def accuracy(y_hat, y):
     if len(y_hat.shape) > 1 and y_hat.shape[1] > 1:
         y_hat = y_hat.argmax(axis=1)
     cmp = y_hat.type(y.dtype) == y
-    return float(cmp.type(y.dtype).sum)
+    return float(cmp.type(y.dtype).sum())
 
 def evaluate_accuracy(net, data_iter):
     if isinstance(net, torch.nn.Module):
         net.eval()
     metric = Accumulator(2)
-    with torch.no_grad:
+    with torch.no_grad():
         for X, y in data_iter:
-            metric.add(accuracy(net(X), y), y.numel())
+            if isinstance(net, torch.nn.Module):
+                y_hat = net(X)
+            else:
+                y_hat = net.forward(X)
+            metric.add(accuracy(y_hat, y), y.numel())
     return metric[0] / metric[1]
 
 def sgd(params, lr, batch_size):
