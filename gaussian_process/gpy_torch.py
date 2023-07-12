64fa2bea4a73475fa57d425f886d6cc22624423d
SaintAugustus <saintaugustus2020@gmail.com>
2023-07-12 17:19:16 -0500

    gaussian process

---
+++ gaussian_process/gpy_torch.py
@@ -0,0 +1,27 @@
+import gpytorch
+
+class ExactGPModel(gpytorch.models.ExactGP):
+    def __init__(self, train_x, train_y, likelihood):
+        super().__init__(train_x, train_y, likelihood)
+        self.mean_module = gpytorch.means.ZeroMean()
+        self.covar_module = gpytorch.kernels.ScaleKernel(
+            gpytorch.kernels.RBFKernel())
+
+    def forward(self, x):
+        mean_x = self.mean_module(x)
+        covar_x = self.covar_module(x)
+        return gpytorch.distributions.MultivariateNormal(mean_x, covar_x)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
